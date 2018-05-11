96f15bd0d7eff62e4797c6a8f6d1b72cbb68bfb1
Steven Crouse <sdcrouse91@gmail.com>
2018-05-11 02:32:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay (beatle, instrument){
   
   for (let n = 0; n < beatle.length; n++){
     hePlaysThis[n] = `${beatle[n]} plays ${instrument[n]}`;
-  }
+  } // You want to make sure that doesn't return something like "Drums plays Ringo Starr" XD
   return hePlaysThis;
 }
 
