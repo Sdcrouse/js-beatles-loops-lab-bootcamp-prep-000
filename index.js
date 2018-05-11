b360f0367db32ef39ef9bb659b4c7502e062502e
Steven Crouse <sdcrouse91@gmail.com>
2018-05-11 02:26:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay (beatle, instrument){
   var hePlaysThis = [];
   
   for (let n = 0; n < beatle.length; n++){
-    hePlaysThis[n] = `${beatle} plays ${instrument}`;
+    hePlaysThis[n] = `${beatle[n]} plays ${instrument[n]}`;
   }
   return hePlaysThis;
 }
